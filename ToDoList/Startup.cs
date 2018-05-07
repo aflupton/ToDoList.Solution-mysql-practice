464fceb40f3be47023bf8910ca783027292d672a
andrew lupton and Brian Henry <brian@>
2018-05-07 09:56:47 -0700

    add changes to database handling and methods

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -40,6 +40,6 @@ namespace ToDoList
     }
     public static class DBConfiguration
     {
-        public static string ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=todo_list;";
+        public static string ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=ToDoList;";
     }
 }
